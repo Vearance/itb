9091ebdfce8e373c0a3900130bc8c29360f68c27
Nathaniel Christian <nathaniel.christian@gmail.com>
2025-09-05 14:33:50 +0700

    feat: sem 3

---
+++ sem-3/prak-alpro2/prak-0/Kali.hs
@@ -0,0 +1,9 @@
+-- HEADER
+module Kali where
+
+-- DEFINISI DAN SPESIFIKASI
+kali :: Int -> Int -> Int
+    -- {kali a b menghasilkan hasil perkalian a dengan b}
+
+-- REALISASI
+kali a b = a * b
---
+++ sem-3/prak-alpro2/prak-0/Pangkat2.hs
@@ -0,0 +1,9 @@
+-- HEADER
+module Pangkat2 where
+
+-- DEFINISI DAN SPESIFIKASI
+pangkat2 :: Int -> Int
+    -- {pangkat2 = angka * angka}
+
+-- REALISASI
+pangkat2 a = a * a
